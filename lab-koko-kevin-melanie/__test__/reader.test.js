d006815b5974802134e1b2eb10d0edea88918f1c
Melanie Downing <sayanything830@gmail.com>
2018-01-18 22:30:53 -0800

    transformers are fun

--- lab-koko-kevin-melanie/__test__/reader.test.js
+++ lab-koko-kevin-melanie/__test__/reader.test.js
@@ -3,6 +3,7 @@
 const reader = require('../lib/reader.js');
 const imagePath = `${__dirname}/asset/bitmap.bmp`;
 const bitmap = require('../lib/bitmap.js');
+const transform = require('../lib/transform.js');
 console.log('this should be a file path', bitmap);
 
 describe('Reader Module', function() {
@@ -35,9 +36,27 @@ describe('Reader Module', function() {
         bitmap.parse(fd, (err, bmp) => {
           if(err) console.error(err);
           console.log(bmp);
-          reader.write(bmp, err => {
-            if(err) return console.error(err);
-            done();
+          // reader.write(bmp, err => {
+          //   if(err) return console.error(err);
+          done();
+          // });
+        });
+      });
+    });
+    it('should write a new file', (done) => {
+      reader.read(imagePath, (err, fd) => {
+        if(err) console.error(err);
+        expect(fd).not.toBeNull();
+        bitmap.parse(fd, (err, bmp) => {
+          if(err) console.error(err);
+          console.log(bmp.pixelArray);
+          transform.blackWhite(bmp, (err, bmpObj) => {
+            console.log('transform function', bmp.pixelArray);
+            if(err) console.error(err);
+            reader.write(bmpObj, err => {
+              if(err) return console.error(err);
+              done();
+            });
           });
         });
       });
--- lab-koko-kevin-melanie/lib/transform.js
+++ lab-koko-kevin-melanie/lib/transform.js
@@ -1,1 +1,83 @@
 'use strict';
+
+const transform = module.exports = {};
+
+transform.random = function(bmp, callback) {
+  for(let i = 0; i < bmp.pixelArray.length; i++) {
+    bmp.pixelArray[i] = Math.floor(Math.random() * Math.floor(255));
+  }
+  callback(null, bmp);
+};
+
+transform.invert = function(bmp, callback) {
+  for(let i = 0; i < bmp.pixelArray.length; i++) {
+    bmp.pixelArray[i] = 255 - bmp.pixelArray[i];
+  }
+  callback(null, bmp);
+};
+
+// transform.reverse = function(bmp, callback) {
+//   let x = bmp.pixelArray.reverse();
+//   bmp.pixelArray = x;
+//   callback(null, bmp);
+// };
+
+transform.boostGreen = function(bmp, callback) {
+  for(let i = 0; i < bmp.pixelArray.length / 4; i += 4) {
+    bmp.pixelArray[i + 1] = 255;
+  }
+  callback(null, bmp);
+};
+
+transform.boostRed = function(bmp, callback) {
+  for(let i = 0; i < bmp.pixelArray.length / 4; i += 4) {
+    bmp.pixelArray[i + 2] = 255;
+  }
+  callback(null, bmp);
+};
+
+transform.boostBlue = function(bmp, callback) {
+  for(let i = 0; i < bmp.pixelArray.length / 4; i += 4) {
+    bmp.pixelArray[i] = 255;
+  }
+  callback(null, bmp);
+};
+
+transform.redChannel = function(bmp, callback) {
+  for(let i = 0; i < bmp.pixelArray.length / 4; i += 4) {
+    bmp.pixelArray[i] = bmp.pixelArray[i + 2];
+    bmp.pixelArray[i + 1] = bmp.pixelArray[i + 2];
+  }
+  callback(null, bmp);
+};
+
+transform.blackWhite = function(bmp, callback) {
+  for(let i = 0; i < bmp.pixelArray.length / 4; i += 4) {
+    let x = ((bmp.pixelArray[i] + bmp.pixelArray[i + 2] + bmp.pixelArray[i + 1]) / 3);
+    bmp.pixelArray[i] = x;
+    bmp.pixelArray[i + 1] = x;
+    bmp.pixelArray[i + 2] = x;
+  }
+  callback(null, bmp);
+};
+
+transform.invert2 = function(bmp, callback) {
+  for(let i = 0; i < bmp.pixelArray.length / 4; i += 4) {
+    bmp.pixelArray[i] = 255 - bmp.pixelArray[i];
+  }
+  callback(null, bmp);
+};
+
+transform.invert3 = function(bmp, callback) {
+  for(let i = 0; i < bmp.pixelArray.length / 3; i += 3) {
+    bmp.pixelArray[i] = 255 - bmp.pixelArray[i];
+  }
+  callback(null, bmp);
+};
+
+transform.invert4 = function(bmp, callback) {
+  for(let i = 0; i < bmp.pixelArray.length / 2; i += 2) {
+    bmp.pixelArray[i] = 255 - bmp.pixelArray[i];
+  }
+  callback(null, bmp);
+};
